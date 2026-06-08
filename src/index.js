37472d97bfe69a6ea849f57e493ae183fc5f8d9b
naaz-josh <naziya.shikalgar@joshsoftware.com>
2026-06-08 07:47:33 +0530

    passed port and env to node server from command line arguments and set those values in server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import express from 'express'
-
+import process from 'process'
 const app =express()
 
 console.log("Hello World")
+ 
+
+let port = process.argv[2]
+let env = process.argv[3]
+
+
 app.get('/',(req,res)=>{
     res.send("All the Habits are retrived succesfully")
 })
 
 
-app.listen(3002,()=>{
+app.listen(port , name,()=>{
     console.log(
-        "Server is listening on port 3002"
+        `Server is listening on port ${port}`
     )
+    console.log(`Server is listening on  ${name} env` )
 })
